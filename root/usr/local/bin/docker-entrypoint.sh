9a403af70c18052ad468eabc511f61a20b054143
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2020-12-20 10:15:32 +0100

    various fixes

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -12,11 +12,6 @@ ConfigureUser
 
 if [ "${1}" == 'mtgapi' ]; then
   INSTALLDIR=/opt/mtgapi
-  if [ "${OS}" == "centos" ]; then
-    PYTHONENV=". /opt/rh/rh-python35/enable"
-  else
-    PYTHONENV="true"
-  fi
   mkdir ${INSTALLDIR}/data
   chown ${MYUSER}:${MYUSER} ${INSTALLDIR}/data
   chmod 750 ${INSTALLDIR}/data
@@ -24,7 +19,8 @@ if [ "${1}" == 'mtgapi' ]; then
   RunDropletEntrypoint
 
   DockLog "Starting app: ${1}"
-  exec su - ${MYUSER} -s /bin/sh -c "${PYTHONENV} && cd ${INSTALLDIR} && uwsgi --ini mtgapi.ini"
+  cd ${INSTALLDIR}
+  exec su-exec ${MYUSER} uwsgi --ini mtgapi.ini
 else
   DockLog "Running command: ${1}"
   exec "$@"
